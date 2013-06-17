742ab7141278c421c9852ca26a3eab5d8297f700
Justin Strickland <Xecantur@gmail.com>
2013-06-17 12:04:00 -0400

    added punctuation

--- test.cpp
+++ test.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 
 int main(int argc, char ** argv) {
-	string test = "Git Hub Testing Repo, learning to rebase and merge";
+	string test = "Git Hub Testing Repo, learning to rebase and merge.";
 	cout << test << endl;
 	return 0;
 }
