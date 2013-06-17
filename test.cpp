3bf1359d78c6579b1bd5e57711ce9d5ef678da91
Justin Strickland <Xecantur@gmail.com>
2013-06-17 12:00:36 -0400

    added a string

--- test.cpp
+++ test.cpp
@@ -5,6 +5,8 @@ using namespace std;
 
 int main(int argc, char ** argv) {
 	string test = "Git Hub Testing Repo, learning to rebase and merge";
+	string rebased = "Git Hub Exhibition, Rebased Edition";
 	cout << test << endl;
+	cout << rebased << endl;
 	return 0;
 }
