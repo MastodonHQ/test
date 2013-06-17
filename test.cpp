0d4c7015648d1dca3c5144cacc404eb03885fb73
Justin Strickland <Xecantur@gmail.com>
2013-06-17 11:56:59 -0400

    added test.cpp

---
+++ test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main(int argc, char ** argv) {
+	string test = "Git Hub Testing Repo, learning to rebase and merge";
+	cout << test << endl;
+	return 0;
+}
